8e0b353c92c04aafe56ed9020c7c42335473c516
Johnny Khalil <khal0988@mylaurier.ca>
2017-11-20 19:13:05 -0500

    Initial commit

---
+++ app/src/main/java/ca/wlu/johnny/akanksha/maptap/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.wlu.johnny.akanksha.maptap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
