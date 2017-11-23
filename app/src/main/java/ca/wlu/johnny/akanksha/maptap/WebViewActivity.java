1ebbbb6a3a45035dcb8f2d963b5f03d0834247c5
Johnny Khalil <khal0988@mylaurier.ca>
2017-11-22 21:46:56 -0500

    added webview activity to open place website

---
+++ app/src/main/java/ca/wlu/johnny/akanksha/maptap/WebViewActivity.java
@@ -0,0 +1,97 @@
+package ca.wlu.johnny.akanksha.maptap;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+/**
+ * Created by johnny on 2017-11-17.
+ */
+
+public class WebViewActivity extends AppCompatActivity {
+
+    private static final String EXTRA_PLACE= "ca.wlu.johnny.akanksha.maptap.place_url";
+
+    private WebView mWebView;
+    private String mURL;
+    private ProgressBar mProgressBar;
+    private TextView mTitleTextView;
+
+    public static Intent newIntent(Context packageContext, String placeUrl) {
+        Intent intent = new Intent(packageContext, WebViewActivity.class);
+        intent.putExtra(EXTRA_PLACE, placeUrl);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        mURL = getIntent().getStringExtra(EXTRA_PLACE);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setMax(100); // WebChromeClient reports in range 0-100
+        mTitleTextView = (TextView) findViewById(R.id.titleTextView);
+
+        setupWebView();
+
+    }
+
+    public void setupWebView() {
+
+        mWebView = (WebView) findViewById(R.id.webview);
+
+        // display the whole webpage in the window
+        mWebView.getSettings().setLoadWithOverviewMode(true);
+        mWebView.getSettings().setUseWideViewPort(true);
+
+        // enable zooming
+        mWebView.getSettings().setBuiltInZoomControls(true);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient());
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView webView, int progress) {
+                if (progress == 100) {
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(progress);
+                }
+            }
+
+            public void onReceivedTitle(WebView webView, String title) {
+                mTitleTextView.setText(title);
+            }
+        });
+
+        mWebView.loadUrl(mURL);
+    }
+
+    // use the device back button for browser history
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+}
